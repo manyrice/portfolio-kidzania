beba3091ca2c819e514f9aefdd1f34d55382edd8
manyrice <alsgml1966@naver.com>
2021-10-03 04:19:52 +0900

    section-4완료

--- docs/common.js
+++ docs/common.js
@@ -1,33 +1,59 @@
-$('.btn-busan').click(function() {
+// 메인 첫화면 부산버튼
+$('.btn-busan').click(function () {
   $('.btn-busan').addClass('active');
   $('.btn-seoul').removeClass('active');
   $('.busan-bg').addClass('active');
   $('.seoul-bg').removeClass('active');
 });
-
-
-$('.btn-seoul').click(function() {
+// 메인 첫화면 서울버튼
+$('.btn-seoul').click(function () {
   $('.btn-seoul').addClass('active');
   $('.btn-busan').removeClass('active');
   $('.busan-bg').removeClass('active');
   $('.seoul-bg').addClass('active');
 });
 
-
 // Section3 스와이퍼 슬라이드
 function Section3__init() {
   const swiper = new Swiper('.section3-slide .swiper-container', {
     loop: true,
     navigation: {
-      nextEl: ".btn-next",
-      prevEl: ".btn-prev",
+      nextEl: ".btn-next1",
+      prevEl: ".btn-prev1",
     },
     on: {
-      slideChange: function() {
+      slideChange: function () {
         $('.section3-slide').attr('data-swiper-slide-index', this.realIndex);
       }
     }
   });
 }
+Section3__init();
 
-PortfolioSlider__init();
+// Section3 스와이퍼 슬라이드
+function Section4__init() {
+  const swiper = new Swiper('.section4-slide .swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+      spaceBetween: 50,
+    navigation: {
+      nextEl: ".btn-next2",
+      prevEl: ".btn-prev2",
+    },
+    on: {
+      slideChange: function () {
+        $('.section4-slide').attr('data-swiper-slide-index', this.realIndex);
+      }
+    }
+  });
+}
+Section4__init();
+
+
+// section-4의 타겟검색창 (야매로..)
+$('.targetage, .targettendency').click(function() {
+  $(this).find('>ul').stop().slideDown(500);
+  $(this).find('>ul').mouseleave(function() {
+    $(this).stop().slideUp(500);
+  });
+});
