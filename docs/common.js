935756bae5d6e6ed13fe947ec5178ba135acf358
manyrice <alsgml1966@naver.com>
2021-10-04 01:32:21 +0900

    나이스스크롤 적용

--- docs/common.js
+++ docs/common.js
@@ -1,4 +1,41 @@
 console.clear();
+
+// 터치가 가능한 디바이스인지 체크하는 로직 시작
+function detectTouchEnabled() {
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    return true;
+  }
+  
+  return false;
+
+  // 아래 로직은 폐기 한다.
+  // 왜냐하면, PC 브라우저라도 터치가 가능한게 있기 때문에, 단순히 터치가능여부를 모바일인지 아닌지로 구분하면 좋지 않다.
+  /*
+  return ( 'ontouchstart' in window ) || 
+    ( navigator.maxTouchPoints > 0 ) || 
+    ( navigator.msMaxTouchPoints > 0 );
+  */
+}
+
+const isTouchEnabled = detectTouchEnabled();
+
+function Touch__init() {
+  if ( isTouchEnabled ) {
+    $('html').addClass('touch-enabled');
+  }
+  else {
+    $('html').addClass('touch-disabled');
+  }
+}
+
+Touch__init();
+// 터치가 가능한 디바이스인지 체크하는 로직 끝
+
+if ( isTouchEnabled == false ) {
+  $('body').niceScroll();
+}
+
+
 // 로딩화면
 $("body").imagesLoaded(function () {
   // 이 부분을 0으로 바뀌면 로딩화면이 안보일 때가 있습니다. 억지로라도 로딩화면을 보여주려면 이 부분을 남겨두세요. 500이 적당합니다.
